df00c1b9dc049efec2d4c3fbc6c4d57ede9ca785
ethanzhz <32383312+ethanzhz@users.noreply.github.com>
2025-09-25 20:08:35 +0800

    标签切换

--- js/class2.py
+++ js/class2.py
@@ -1,1 +1,22 @@
+# student number  chinese english maths
+
+
 class Student :
+    def __init__(self, name, number, chinese, english, maths):
+        self.name = name
+        self.number = number
+        self.chinese = chinese
+        self.english = english
+        self.maths = maths
+        
+    def info(self):
+        print(f'姓名：{self.name}学号：{self.number}语文：{self.chinese}数学：{self.maths}英语：{self.english}')
+        
+        
+        
+a = Student('张逸', '93', '68', '74', '59')
+
+a.info()
+
+l = list()
+print(l)
